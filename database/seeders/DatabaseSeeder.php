f441e13785fd521f50d468bd50d2fd839942f08d
Alberto <dany.canaviri2022@gmail.com>
2024-07-29 18:03:08 -0400

    Uso de seeders para crear algunos registros en la tabla users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +20,29 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            'name' => 'Administrador',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('12345678')
+        ]);
+
+        User::create([
+            'name' => 'Secretaria',
+            'email' => 'secretaria@admin.com',
+            'password' => Hash::make('12345678')
+        ]);
+
+        User::create([
+            'name' => 'Doctor1',
+            'email' => 'doctor1@admin.com',
+            'password' => Hash::make('12345678')
+        ]);
+
+        User::create([
+            'name' => 'Paciente1',
+            'email' => 'paciente1@admin.com',
+            'password' => Hash::make('12345678')
+        ]);
     }
 }
