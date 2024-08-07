52073c1d88cb109eb8a9ae6dbfe74a09ef9d7f60
Alberto <dany.canaviri2022@gmail.com>
2024-08-06 23:14:41 -0400

    Se creo el modelo, controlador y la migración para el modulo Secretarias

---
+++ app/Http/Controllers/SecretariaController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Secretaria;
+use Illuminate\Http\Request;
+
+class SecretariaController extends Controller
+{
+    public function index() {
+    }
+
+    public function create() {
+    }
+
+    public function store(Request $request) {
+    }
+
+    public function show(Secretaria $secretaria) {
+    }
+
+    public function edit(Secretaria $secretaria) {
+    }
+
+    public function update(Request $request, Secretaria $secretaria) {
+    }
+
+    public function destroy(Secretaria $secretaria) {
+    }
+}
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -13,11 +13,27 @@
                 </ol>
             </div>
         </div>
+
+        <hr>
     </div>
 @endsection
 
 @section('content')
-    <div class="row">
-        
+    
+    <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-info">
+            <div class="inner">
+                <h3>{{ $total_usuarios }}</h3>
+                <p>Registros de Usuarios</p>
+            </div>
+            <a href="{{ url('admin/usuarios/create')}}">
+                <div class="icon">
+                    <i class="ion ion-person-add"></i>
+                </div>
+            </a>
+            <a href="{{ url('admin/usuarios')}}" class="small-box-footer">Más información <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
     </div>
+    
 @endsection
