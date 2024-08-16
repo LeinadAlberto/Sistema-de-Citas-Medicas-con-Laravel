914cc5dba231dcffd045725a390f58fe863be3c6
Alberto <dany.canaviri2022@gmail.com>
2024-08-16 00:35:21 -0400

    Se realizo la relación de modelos Consultorio, Doctores y Horarios

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Doctor extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nombres',
+        'apellidos',
+        'telefono',
+        'licencia_medica',
+        'especialidad',
+        'user_id'
+    ];
+
+    // Un doctor pertenece  a un consultorio
+    public function consultorio() {
+
+        return $this->belongsTo(Consultorio::class);
+
+    }
+
+    // Un doctor tiene muchos horarios
+    public function horarios() {
+
+        return $this->hasMany(Horario::class);
+
+    }
 }
--- app/Models/Horario.php
+++ app/Models/Horario.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class Horario extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'dia',
+        'hora_inicio',
+        'hora_fin',
+        'doctor_id',
+        'consultorio_id'
+    ];
+
+    // Un horario pertenece  a un doctor
+    public function doctor() {
+
+        return $this->belongsTo(Doctor::class);
+
+    }
+
+    // Un horario pertenece  a un consultorio
+    public function consultorio() {
+
+        return $this->belongsTo(Consultorio::class);
+
+    }
+
+
 }
