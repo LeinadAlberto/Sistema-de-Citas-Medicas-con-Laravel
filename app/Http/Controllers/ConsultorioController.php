19269af5d8d481927445285745a4f552b69426ff
Alberto <dany.canaviri2022@gmail.com>
2024-08-16 11:58:20 -0400

    Se finalizo la funcionalidad para listar registros de Consultorios

--- app/Http/Controllers/ConsultorioController.php
+++ app/Http/Controllers/ConsultorioController.php
@@ -7,59 +7,35 @@ use Illuminate\Http\Request;
 
 class ConsultorioController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
+    public function index() {
+        
+        $consultorios = Consultorio::all();
+
+        return view('admin.consultorios.index', compact('consultorios'));
+
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+    public function create() {
+        
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
+    public function store(Request $request) {
+        
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Consultorio $consultorio)
-    {
-        //
+    public function show(Consultorio $consultorio) {
+        
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Consultorio $consultorio)
-    {
-        //
+    public function edit(Consultorio $consultorio) {
+        
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Consultorio $consultorio)
-    {
-        //
+    public function update(Request $request, Consultorio $consultorio) {
+        
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Consultorio $consultorio)
-    {
-        //
+    public function destroy(Consultorio $consultorio) {
+        
     }
 }
